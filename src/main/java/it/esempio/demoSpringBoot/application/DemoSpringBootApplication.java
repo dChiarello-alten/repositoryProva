d47f0a9a5527a7c47a8e8174dd2d732a3cbbe003
IL DAVE <davide.chiarello@alten.it>
2020-04-16 13:19:13 +0200

    Aggiunto controller di esempio

--- src/main/java/it/esempio/demoSpringBoot/application/DemoSpringBootApplication.java
+++ src/main/java/it/esempio/demoSpringBoot/application/DemoSpringBootApplication.java
@@ -1,4 +1,4 @@
-package it.esempio.demoSpringBoot;
+package it.esempio.demoSpringBoot.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
