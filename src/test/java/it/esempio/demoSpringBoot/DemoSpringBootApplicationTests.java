7390e141bdf5d24b5da216fb0404e32270035133
IL DAVE <davide.chiarello@alten.it>
2020-04-16 13:09:37 +0200

    Commit dello scheletro progettuale

---
+++ src/test/java/it/esempio/demoSpringBoot/DemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package it.esempio.demoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
